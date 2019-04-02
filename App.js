171eafa5e3d92f134a540ceb8b63540d60ef68ff
nbright <kms3700@gmail.com>
2019-04-02 09:43:40 +0900

    mainscreen

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import MainScreen from './Components/MainScreen';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+
+
+const AppStackNavigator = createStackNavigator({
+  Main: {
+    screen: MainScreen
   }
-}
+})
+
+export default createAppContainer(AppStackNavigator);
 
 const styles = StyleSheet.create({
   container: {
